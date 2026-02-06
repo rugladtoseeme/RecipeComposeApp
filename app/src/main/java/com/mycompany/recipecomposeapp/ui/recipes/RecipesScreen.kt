16a4a0a01c042cb326f22c03e89d7078b6839bab
rugladtoseeme <mrgrafen@mail.ru>
2026-02-06 18:12:07 +0300

    RCA-15-recipes-list-screen Экран со списком рецептов

--- app/src/main/java/com/mycompany/recipecomposeapp/ui/recipes/RecipesScreen.kt
+++ app/src/main/java/com/mycompany/recipecomposeapp/ui/recipes/RecipesScreen.kt
@@ -1,34 +1,61 @@
 package com.mycompany.recipecomposeapp.ui.recipes
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.dp
+import com.mycompany.recipecomposeapp.data.model.RecipeDto
+import com.mycompany.recipecomposeapp.data.model.toUiModel
+import com.mycompany.recipecomposeapp.data.repository.RecipesRepositoryStub
 import com.mycompany.recipecomposeapp.ui.core.ui.ScreenHeader
 
 @Composable
-fun RecipesScreen(drawableResId: Int, headerText: String, modifier: Modifier = Modifier) {
+fun RecipesScreen(
+    categoryId: Int,
+    categoryTitle: String,
+    drawableResId: Int,
+    modifier: Modifier = Modifier
+) {
+
+    var recipes by remember { mutableStateOf(listOf<RecipeDto>()) }
+
+    var isLoading by remember { mutableStateOf(false) }
+
+    LaunchedEffect(categoryId) {
+        isLoading = true
+        try {
+            recipes = RecipesRepositoryStub.getRecipesByCategoryId(categoryId)
+        } finally {
+            isLoading = false
+        }
+    }
 
     Column(modifier = modifier.fillMaxWidth()) {
         ScreenHeader(
             drawableResId,
-            headerText
+            categoryTitle
         )
 
-        Spacer(Modifier.weight(weight = 1f))
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
 
-        Text(
-            text = "Скоро здесь будет список рецептов",
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally),
-            fontSize = 30.sp
-        )
-
-        Spacer(Modifier.weight(weight = 1f))
+            items(items = recipes, key = { it.id }) { recipe ->
+                RecipeItem(recipe = recipe.toUiModel(), onRecipeClick = {})
+            }
+        }
     }
-
 }
