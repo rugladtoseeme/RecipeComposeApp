20157767cf6a6182c89763df2076370ee263c713
rugladtoseeme <mrgrafen@mail.ru>
2026-02-09 14:47:21 +0300

    RCA-16-add-jetpack-navigation Внедрить Jetpack Navigation

--- app/src/main/java/com/mycompany/recipecomposeapp/RecipesApp.kt
+++ app/src/main/java/com/mycompany/recipecomposeapp/RecipesApp.kt
@@ -1,24 +1,25 @@
 package com.mycompany.recipecomposeapp
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.mycompany.recipecomposeapp.data.repository.RecipesRepositoryStub
 import com.mycompany.recipecomposeapp.ui.categories.CategoriesScreen
 import com.mycompany.recipecomposeapp.ui.favorites.FavoritesScreen
 import com.mycompany.recipecomposeapp.ui.navigation.BottomNavigation
+import com.mycompany.recipecomposeapp.ui.navigation.Destination
 import com.mycompany.recipecomposeapp.ui.recipes.RecipesScreen
 import com.mycompany.recipecomposeapp.ui.theme.RecipeComposeAppTheme
 
@@ -26,87 +27,68 @@ import com.mycompany.recipecomposeapp.ui.theme.RecipeComposeAppTheme
 fun RecipesApp() {
     RecipeComposeAppTheme {
 
-        var currentScreen by remember { mutableStateOf(ScreenId.CATEGORIES_LIST) }
-
-        var selectedCategoryId by remember { mutableStateOf<Int?>(null) }
-
         var selectedCategoryTitle by remember { mutableStateOf("") }
 
-        val onCategoryClick: (Int) -> Unit = { categoryId ->
-            selectedCategoryId = categoryId
-            selectedCategoryTitle =
-                RecipesRepositoryStub.getCategoryById(categoryId).title.uppercase()
-            currentScreen = ScreenId.RECIPES_LIST
-        }
+        val navController = rememberNavController()
 
         Scaffold(
             modifier = Modifier.fillMaxSize(),
             bottomBar = {
                 BottomNavigation(
-                    onFavoritesClick = { currentScreen = ScreenId.FAVORITES },
-                    onCategoriesClick = { currentScreen = ScreenId.CATEGORIES_LIST }
+                    onFavoritesClick = {
+                        navController.navigate("favorites") {
+                            popUpTo("favorites") { inclusive = true }
+                            launchSingleTop = true
+                        }
+                    },
+                    onCategoriesClick = {
+                        navController.navigate("categories") {
+                            popUpTo("favorites") { inclusive = true }
+                            launchSingleTop = true
+                        }
+                    }
                 )
             }
         ) { paddingValues ->
 
-            when (currentScreen) {
-                ScreenId.FAVORITES -> Box(
-                    contentAlignment = Alignment.Center,
-                    modifier = Modifier.fillMaxSize()
-                ) {
+            NavHost(
+                navController = navController,
+                startDestination = "categories"
+            ) {
+
+                composable(route = "categories") {
+                    CategoriesScreen(
+                        onCategoryClick = { categoryId ->
+                            selectedCategoryTitle =
+                                RecipesRepositoryStub.getCategoryById(categoryId).title.uppercase()
+                            navController.navigate(Destination.Recipes.createRoute(categoryId)) {
+                                launchSingleTop = true
+                            }
+                        },
+                        drawableResId = R.drawable.img_categories_header,
+                        headerText = "КАТЕГОРИИ",
+                        modifier = Modifier.padding(paddingValues)
+                    )
+                }
+
+                composable(route = "favorites") {
                     FavoritesScreen(
                         drawableResId = R.drawable.img_favorites_header,
                         headerText = "ИЗБРАННОЕ",
-                        Modifier.padding(paddingValues)
+                        modifier = Modifier.padding(paddingValues)
                     )
                 }
 
-                ScreenId.CATEGORIES_LIST -> Box(modifier = Modifier.fillMaxWidth()) {
-
-                    CategoriesScreen(
-                        drawableResId = R.drawable.img_categories_header,
-                        headerText = "КАТЕГОРИИ",
-                        modifier = Modifier.padding(paddingValues),
-                        onCategoryClick = onCategoryClick
-                    )
-                }
-
-                ScreenId.SPLASH_SCREEN -> Box(
-                    contentAlignment = Alignment.Center,
-                    modifier = Modifier.fillMaxSize()
-                ) {
-                    Text(
-                        text = "Экран загрузки",
-                        modifier = Modifier
-                            .padding(paddingValues)
-                            .align(Alignment.Center),
-                        fontSize = 50.sp
-                    )
-                }
-
-                ScreenId.RECIPE -> Box(
-                    contentAlignment = Alignment.Center,
-                    modifier = Modifier.fillMaxSize()
-                ) {
-                    Text(
-                        text = "Рецепт",
-                        modifier = Modifier
-                            .padding(paddingValues)
-                            .align(Alignment.Center),
-                        fontSize = 50.sp
-                    )
-                }
-
-                ScreenId.RECIPES_LIST -> Box(
-                    contentAlignment = Alignment.Center,
-                    modifier = Modifier.fillMaxSize()
-                ) {
-
+                composable(
+                    route = Destination.Recipes.route,
+                    arguments = listOf(navArgument("categoryId") { type = NavType.IntType })
+                ) { backStackEntry ->
+                    val categoryId = backStackEntry.arguments?.getInt("categoryId") ?: 0
                     RecipesScreen(
-                        categoryId = selectedCategoryId ?: error("Category ID is required"),
-                        drawableResId = R.drawable.img_recipes_list_header,
+                        categoryId = categoryId,
                         categoryTitle = selectedCategoryTitle,
-                        modifier = Modifier.padding(paddingValues),
+                        drawableResId = R.drawable.img_recipes_list_header,
+                        modifier = Modifier.padding(paddingValues)
                     )
                 }
             }
