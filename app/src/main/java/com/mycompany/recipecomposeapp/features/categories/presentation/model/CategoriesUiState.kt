28f0bd10444c755db986bf9611a32e3590f7ef1b
rugladtoseeme <mrgrafen@mail.ru>
2026-03-05 16:03:26 +0300

    RCA-26-add-viewmodel Первая ViewModel в Jetpack Compose

---
+++ app/src/main/java/com/mycompany/recipecomposeapp/features/categories/presentation/model/CategoriesUiState.kt
@@ -0,0 +1,7 @@
+package com.mycompany.recipecomposeapp.features.categories.presentation.model
+
+data class CategoriesUiState(
+    val categories: List<CategoryUiModel> = emptyList(),
+    val isLoading: Boolean = false,
+    val error: String? = null
+)
