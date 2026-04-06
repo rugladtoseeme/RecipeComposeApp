e46d9d317410ec43d6a3aee71405d5ccb91876dc
rugladtoseeme <mrgrafen@mail.ru>
2026-04-06 12:09:14 +0300

    RCA-39-recipe-entuty-and-typeconverter

--- app/src/main/java/com/mycompany/recipecomposeapp/data/database/RecipesDatabase.kt
+++ app/src/main/java/com/mycompany/recipecomposeapp/data/database/RecipesDatabase.kt
@@ -4,16 +4,23 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.mycompany.recipecomposeapp.data.database.converter.Converter
 import com.mycompany.recipecomposeapp.data.database.dao.CategoryDao
+import com.mycompany.recipecomposeapp.data.database.dao.RecipeDao
 import com.mycompany.recipecomposeapp.data.database.entity.CategoryEntity
+import com.mycompany.recipecomposeapp.data.database.entity.RecipeEntity
 
 @Database(
-    entities = [CategoryEntity::class],
-    version = 1,
+    entities = [CategoryEntity::class, RecipeEntity::class],
+    version = 2,
     exportSchema = false
 )
+@TypeConverters(value = [Converter::class])
 abstract class RecipesDatabase() : RoomDatabase() {
     abstract fun categoryDao(): CategoryDao
+
+    abstract fun recipeDao(): RecipeDao
 
     companion object {
         fun buildDatabase(context: Context): RecipesDatabase = Room.databaseBuilder(
