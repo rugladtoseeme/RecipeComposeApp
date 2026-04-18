0d4235b36a6ac3cc85f3a2c5ca954f8e244c7e8c
rugladtoseeme <mrgrafen@mail.ru>
2026-04-18 15:39:43 +0300

    RCA-45-di-without-hilt правки

--- app/src/main/java/com/mycompany/recipecomposeapp/di/RecipeApplication.kt
+++ app/src/main/java/com/mycompany/recipecomposeapp/di/RecipeApplication.kt
@@ -2,9 +2,11 @@ package com.mycompany.recipecomposeapp.di
 
 import android.app.Application
 
-class RecipeApplication: Application() {
+class RecipeApplication : Application() {
 
     lateinit var appContainer: AppContainer
+        private set
+
 
     override fun onCreate() {
         super.onCreate()
